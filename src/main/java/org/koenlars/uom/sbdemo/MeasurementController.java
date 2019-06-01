dba7e47312f48669d060208134dda6f6fbf59f53
Lars Rosenquist <lrosenquist@pivotal.io>
2019-06-01 14:58:02 +0200

    Parameterized Unit conversion

--- src/main/java/org/koenlars/uom/sbdemo/MeasurementController.java
+++ src/main/java/org/koenlars/uom/sbdemo/MeasurementController.java
@@ -3,21 +3,23 @@ package org.koenlars.uom.sbdemo;
 import javax.measure.MetricPrefix;
 import javax.measure.Unit;
 import javax.measure.UnitConverter;
-import javax.measure.quantity.Length;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import tech.units.indriya.unit.Units;
 
 @RestController
 public class MeasurementController {
-	@GetMapping("/meters/{number}")
-	public Number doTransformIntoMeters(@PathVariable("number") Number number) {
-		Unit<Length> sourceUnit = MetricPrefix.KILO(Units.METRE);
-		Unit<Length> targetUnit = Units.METRE;
-		UnitConverter converter = sourceUnit.getConverterTo(targetUnit);
-		return converter.convert(number); 
+	@PostMapping("/meters")
+	public Measurement doTransformIntoMeters(@RequestBody Measurement measurement) throws Exception	{
+		Unit<?> sourceUnit = Units.getInstance().getUnit("m").prefix(MetricPrefix.valueOf("KILO"));
+		Unit<?> targetUnit = Units.getInstance().getUnit("m");
+		UnitConverter converter = sourceUnit.getConverterToAny(targetUnit);
+		Number result = converter.convert(measurement.getValue());
+		Measurement resultMeasurement = new Measurement();
+		resultMeasurement.setValue(result);
+		return resultMeasurement;
 	}
 }
