e3c5bdded40eefdaa9d320c9a9669af12c10f8ff
Lars Rosenquist <lrosenquist@pivotal.io>
2019-06-01 13:39:19 +0200

    Added Units API

--- src/main/java/org/koenlars/uom/sbdemo/MeasurementController.java
+++ src/main/java/org/koenlars/uom/sbdemo/MeasurementController.java
@@ -1,14 +1,23 @@
 package org.koenlars.uom.sbdemo;
 
+import javax.measure.MetricPrefix;
+import javax.measure.Unit;
+import javax.measure.UnitConverter;
+import javax.measure.quantity.Length;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import tech.units.indriya.unit.Units;
 
 @RestController
 public class MeasurementController {
 	@GetMapping("/meters/{number}")
-	public int doTransformIntoMeters(@PathVariable("number") String number) {
-		return new Integer(number) * 1000; 
+	public Number doTransformIntoMeters(@PathVariable("number") Number number) {
+		Unit<Length> sourceUnit = MetricPrefix.KILO(Units.METRE);
+		Unit<Length> targetUnit = Units.METRE;
+		UnitConverter converter = sourceUnit.getConverterTo(targetUnit);
+		return converter.convert(number); 
 	}
 }
