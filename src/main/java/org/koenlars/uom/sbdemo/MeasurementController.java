ac3cb9227c04dccca5fc56a52993eab00592607b
Koenus <koaben@gmail.com>
2019-06-01 15:44:08 +0100

    smart validation

--- src/main/java/org/koenlars/uom/sbdemo/MeasurementController.java
+++ src/main/java/org/koenlars/uom/sbdemo/MeasurementController.java
@@ -1,6 +1,8 @@
 package org.koenlars.uom.sbdemo;
 
+import javax.measure.IncommensurableException;
 import javax.measure.MetricPrefix;
+import javax.measure.UnconvertibleException;
 import javax.measure.Unit;
 import javax.measure.UnitConverter;
 
@@ -21,18 +23,33 @@ public class MeasurementController {
 	 * @return
 	 * @throws Exception
 	 */
-	@PostMapping("/measurement")
+	@PostMapping("/conversion")
 	public Measurement doTransformFromPrefix(@RequestBody Measurement measurement) throws Exception	{
+		if (measurement.getTargetPrefix()==null) {
+			measurement.setTargetPrefix(measurement.getPrefix());
+		}
 		
-		Unit<?> sourceUnit = Units.getInstance().getUnit(measurement.getUnit()).prefix(MetricPrefix.valueOf(measurement.getPrefix()));
-		Unit<?> targetUnit = Units.getInstance().getUnit(measurement.getTargetUnit()).prefix(MetricPrefix.valueOf(measurement.getTargetPrefix()));
-		UnitConverter converter = sourceUnit.getConverterToAny(targetUnit);
-		Number result = converter.convert(measurement.getValue());
+		Unit<?> sourceUnit = createUnit(measurement.getUnit(),measurement.getPrefix());
+		Unit<?> targetUnit = createUnit(measurement.getTargetUnit(),measurement.getTargetPrefix());
+		
 		Measurement resultMeasurement = new Measurement();
-		resultMeasurement.setValue(result);
+		resultMeasurement.setValue(conversionOfUnits(sourceUnit, targetUnit, measurement.getValue()));
 		resultMeasurement.setUnit(measurement.getUnit());
-		resultMeasurement.setPrefix(measurement.getPrefix());
-		
+		resultMeasurement.setPrefix(measurement.getTargetPrefix());
 		return resultMeasurement;
+	}
+	
+	private Unit<?> createUnit( String unit, String prefix) throws UnconvertibleException, IncommensurableException {
+		Unit<?> targetUnit = Units.getInstance().getUnit(unit);
+		if (prefix!=null) {
+			targetUnit = targetUnit.prefix(MetricPrefix.valueOf(prefix));
+		}
+		return targetUnit; 
+	}
+	
+	private Number conversionOfUnits(Unit<?> sourceUnit, Unit<?> targetUnit, Number value) throws UnconvertibleException, IncommensurableException {
+		UnitConverter converter = sourceUnit.getConverterToAny(targetUnit);
+		Number result = converter.convert(value);
+		return result;
 	}
 }
